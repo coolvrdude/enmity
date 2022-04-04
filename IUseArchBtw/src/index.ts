ee726f1ef5890eadb15285766f9e6ab0524047ee
beerpiss <lacvtg.a1.2023@gmail.com>
2022-04-04 20:06:44 +0700

    plugin: i use arch btw

---
+++ IUseArchBtw/src/index.ts
@@ -0,0 +1,48 @@
+import { Command, EnmitySectionID, registerCommands, ApplicationCommandInputType, ApplicationCommandOptionType, ApplicationCommandType } from "enmity-api/commands";
+import { registerPlugin } from "enmity-api/plugins";
+
+registerPlugin({
+  name: "i use arch btw",
+  commands: [],
+
+  onStart() {
+    const btw: Command = {
+      id: "i-use-arch-btw-command",
+      applicationId: EnmitySectionID,
+
+      name: "btw",
+      displayName: "btw",
+
+      description: "Appends \"i use arch btw\" to the end of your message.",
+      displayDescription: "Appends \"i use arch btw\" to the end of your message.",
+      
+      type: ApplicationCommandType.Chat,
+      inputType: ApplicationCommandInputType.BuiltInText,
+      
+      options: [{
+        name: "text",
+        displayName: "text",
+
+        description: "Text to send",
+        displayDescription: "Text to send",
+        
+        type: ApplicationCommandOptionType.String,
+        required: true
+      }],
+    
+      execute: function (args, message) {
+        const text = args[0].value;
+    
+        return {
+          content: `${text} i use arch btw`
+        };
+      }
+    }
+
+    this.commands.push(btw);
+  },
+
+  onStop() {
+    this.commands = [];
+  }
+});
