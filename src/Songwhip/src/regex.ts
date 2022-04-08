5ff8506cf88ec542737b056d8eb5dbca6a7eaf0b
beerpiss <lacvtg.a1.2023@gmail.com>
2022-04-08 21:19:54 +0700

    Songwhip: split regex into multiple parts so I don't off myself

--- src/Songwhip/src/regex.ts
+++ src/Songwhip/src/regex.ts
@@ -1,1 +1,10 @@
-export const MusicRegex = new RegExp("(https?://(?:open.spotify.com/track/[A-Za-z0-9]+|music.apple.com/[[a-zA-Z][a-zA-Z]]?/album/[a-zA-Z\\d%\\(\\)-]+/[\\d]{1,10}\\?i=[\\d]{1,15}|soundcloud.com/\\S+|listen.tidal.com/track/\\d+|(?:music.|www.)?youtube.com/watch\\S*?v=[0-9a-zA-Z_-]{11}\\S*|www.deezer.com/track/\\d+|music.amazon.com/albums/[0-9A-Z]+\\?trackAsin=[0-9A-Z]+))", "gi")
+const SubMusicRegex: string[] = [
+    "open.spotify.com/track/[A-Za-z0-9]+",
+    "music.apple.com/[[a-zA-Z][a-zA-Z]]?/album/[a-zA-Z\\d%\\(\\)-]+/[\\d]{1,10}\\?i=[\\d]{1,15}",
+    "soundcloud.com/\\S+",
+    "listen.tidal.com/track/\\d+",
+    "(?:music.|www.)?youtube.com/watch\\S*?v=[0-9a-zA-Z_-]{11}\\S*",
+    "www.deezer.com/track/\\d+",
+    "music.amazon.com/albums/[0-9A-Z]+\\?trackAsin=[0-9A-Z]+",
+];
+export const MusicRegex = new RegExp(`(https?://(?:${SubMusicRegex.join("|")}))`, "gi");
